aa734f7a7f88b4c5df70da89eb0cd29aeb406670
RyanMrts <ryanmartinssilva2006@gmail.com>
2023-04-30 17:27:05 -0300

    exercicio feito

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿
+string resposta;
+Console.WriteLine("O objeto esta se mexendo(S/N)?");
+ string mexendo = (Console.ReadLine()!.Substring(0, 1).ToUpper()); 
+
+Console.WriteLine("Deveria?");
+ string deveria = (Console.ReadLine()!.Substring(0, 1).ToUpper()); 
+if (mexendo == "N") 
+{
+    if (deveria == "S") 
+    {
+        Console.ForegroundColor = ConsoleColor.DarkBlue;
+        resposta = "Use WD-40.";
+        Console.ResetColor();
+    }
+    else
+    {
+        resposta = "Ótimo !";
+    }
+}
+else
+{
+    if (deveria == "N")
+    {
+        Console.ForegroundColor = ConsoleColor.DarkGray;
+        resposta = "Use Silver Tape.";
+        Console.ResetColor();
+    }
+    else
+    {
+        resposta = "Ótimo !";
+    }
+}
+Console.WriteLine(resposta);
